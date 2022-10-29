796d3f7a2599f6768a986faf7f3b22a84ee1ded0
Mrsly91 <116326133+Mrsly91@users.noreply.github.com>
2022-10-29 09:59:37 +0000

    Update models.py

--- mysite/musicapp/models.py
+++ mysite/musicapp/models.py
@@ -7,25 +7,27 @@ from datetime import datetime
 class Artiste(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
 
     def _str_(self):
         return self.first_name
 
 
 class Song(models.Model):
-    artiste = models.ForeignKey(to=Artiste,null=True, on_delete=models.CASCADE)
+    artiste = models.ForeignKey(Artiste,null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=25)
-    date_released = models.DateField(default=datetime.today)
-    likes = models.IntegerField()
-
+    date_released = models.DateField()
+    likes = models.PositveIntegerField(default=0)
+    #artiste_id = models.CharField(max_length=25)
+    
     def _str_(self) -> str:
         return self.title
 
 
 class Lyric(models.Model):
-    song_id = models.ForeignKey(Song, null=True, on_delete=models.CASCADE)
-    content = models.CharField(max_length=800)
-
+    song = models.ForeignKey(Song, null=True, on_delete=models.CASCADE)
+    content = models.TextField()
+    #song_id = models.CharField(max_length=800)
+    
     def _str_(self) -> str:
         return self.content
